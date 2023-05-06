f5d9d37246a6cf9c0ae8d7dad1728a71934ce748
Yoann <ybuzenet@gmail.com>
2023-05-06 21:34:27 +0200

    working on types

--- front/app/posts/read/[[...postURL]]/PostTwitterDisplay.tsx
+++ front/app/posts/read/[[...postURL]]/PostTwitterDisplay.tsx
@@ -1,10 +1,21 @@
 "use client";
 import { useEffect } from "react";
 
+type TwitterWindow = Window &
+  typeof globalThis & {
+    twttr: {
+      widgets: {
+        load: () => void;
+      };
+    };
+  };
+
 const PostTwitterDisplay = () => {
+  const windowLoaded = window as TwitterWindow;
+
   // Without this, the tweet can't load if using next client-side router
   useEffect(() => {
-    window.twttr.widgets.load();
+    windowLoaded.twttr.widgets.load();
   }, []);
   return <></>;
 };
